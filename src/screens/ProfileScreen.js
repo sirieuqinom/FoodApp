67fab294450cd6f27d1512d4a0a3fbe67e83850b
Monique Llorca <60698833+sirieuqinom@users.noreply.github.com>
2021-05-16 18:04:59 +0800

    Update ProfileScreen.js

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import { Image, View, Text, StyleSheet, Alert, SafeAreaView} from 'react-native';
+import { Image, View, Text, StyleSheet, Alert, SafeAreaView, Button} from 'react-native';
 import { userProfile } from '../backend/userProfile';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { currentUN, currentN, currentEM, currentPW, currentAD } from '../backend/Verification'
 import home from '../styles/home';
 import { icons} from '../constants/index';
+import { FontAwesome, Ionicons, AntDesign } from '@expo/vector-icons';
+import Modal from "react-native-modal";
 
 
 function ProfileScreen(props) {
-//admin
+    
+    
+    //admin
     if (currentAD == true){
     return(
         <SafeAreaView style={home.container}>
-        
+        <View style={{ flex:1, flexDirection: 'column', height: 150, backgroundColor: 'transparent'}}>
         <View style={{ flexDirection: 'column', height: 150, backgroundColor: 'transparent'}}>
         <Image source={icons.logo} style={{ alignSelf: 'center',width: 260, height: 158, marginLeft: 10}}/>    
         </View> 
@@ -23,23 +27,36 @@ function ProfileScreen(props) {
         <Text style ={{marginTop: 8 , marginLeft: 8, marginBottom: 4, color: '##7f8fa6', fontSize: 15}}>Name</Text>
         <Text style ={{alignSelf:'center', color: '#192a56', fontSize: 18}}>{currentN}</Text>   
         </View>
-        <View style={{ flexDirection: 'column', height: 90, borderBottomWidth: 3,borderBottomColor: '#7f8fa6', marginBottom:8 }}>
+        <View style={{ flexDirection: 'column', height: 90, borderBottomWidth: 3,borderBottomColor: '#7f8fa6', marginBottom:10 }}>
         <Text style ={{marginTop: 8 , marginLeft: 8, marginBottom: 4, color: '#192a56', fontSize: 15}}>Email</Text>
         <Text style={{alignSelf:'center', color: '#192a56', fontSize: 18}}>{currentEM}</Text>   
         </View>
+        
+        <TouchableOpacity>
         <View style={{ flexDirection: 'column', height: 70, backgroundColor: '#353b48', marginBottom: 3}}>
-        <Text style ={{margin: 20 , color: '#f5f6fa', fontSize: 20}}>Change Password</Text> 
+        <Text style ={{margin: 20 , color: '#f5f6fa', fontSize: 20}}><AntDesign name="key" size={24} color="#f5f6fa" />  Change Password </Text> 
         </View> 
+        </TouchableOpacity>
+        
+        <TouchableOpacity>
         <View style={{ flexDirection: 'column', height: 70, backgroundColor: '#353b48', marginBottom: 3}}>
-        <Text style ={{margin: 20 , color: '#f5f6fa', fontSize: 20}}>Add New Products</Text> 
+        <Text style ={{margin: 20 , color: '#f5f6fa', fontSize: 20}}><Ionicons name="add-sharp" size={24} color="#f5f6fa" />  Add Products</Text> 
         </View>
+        </TouchableOpacity>
+        
+        <TouchableOpacity>
         <View style={{ flexDirection: 'column', height: 70, backgroundColor: '#353b48', marginBottom: 8}}>
-        <Text style ={{margin: 20 , color: '#f5f6fa', fontSize: 20}}>About US</Text> 
+        <Text style ={{margin: 20 , color: '#f5f6fa', fontSize: 20}}><Ionicons name="ios-information-circle-outline" size={24} color="#f5f6fa" />  About Us</Text> 
         </View>
+        </TouchableOpacity>
+        
+        <TouchableOpacity>
         <View style={{ flexDirection: 'column', height: 70, backgroundColor: '#2f3640'}}>
         <Text style ={{margin: 20 , color: '#f5f6fa', fontSize: 20, alignSelf: 'center'}}>Logout</Text> 
         </View>
-
+        </TouchableOpacity>
+        
+        </View>
         </SafeAreaView>
     )
     }else{
@@ -61,20 +78,36 @@ function ProfileScreen(props) {
         <Text style ={{marginTop: 8 , marginLeft: 8, marginBottom: 4, color: '#192a56', fontSize: 15}}>Email</Text>
         <Text style={{alignSelf:'center', color: '#192a56', fontSize: 18}}>{currentEM}</Text>   
         </View>
+        
+        <TouchableOpacity>
         <View style={{ flexDirection: 'column', height: 70, backgroundColor: '#353b48', marginBottom: 3}}>
-        <Text style ={{margin: 20 , color: '#f5f6fa', fontSize: 20}}>Change Password</Text> 
+        <Text style ={{margin: 20 , color: '#f5f6fa', fontSize: 20}}><AntDesign name="key" size={24} color="#f5f6fa" />  Change Password </Text> 
         </View> 
+        </TouchableOpacity>
+
+        <TouchableOpacity>
         <View style={{ flexDirection: 'column', height: 70, backgroundColor: '#353b48', marginBottom: 3}}>
-        <Text style ={{margin: 20 , color: '#f5f6fa', fontSize: 20}}>Order History</Text> 
+        <Text style ={{margin: 20 , color: '#f5f6fa', fontSize: 20}}><FontAwesome name="history" size={24} color="#f5f6fa" />  Order History</Text> 
         </View>
+        </TouchableOpacity>
+        
+        <TouchableOpacity>
         <View style={{ flexDirection: 'column', height: 70, backgroundColor: '#353b48', marginBottom: 8}}>
-        <Text style ={{margin: 20 , color: '#f5f6fa', fontSize: 20}}>About US</Text> 
+        <Text style ={{margin: 20 , color: '#f5f6fa', fontSize: 20}}><Ionicons name="ios-information-circle-outline" size={24} color="#f5f6fa" />  About Us</Text> 
         </View>
+        </TouchableOpacity>
+        
+        <TouchableOpacity>
         <View style={{ flexDirection: 'column', height: 70, backgroundColor: '#2f3640'}}>
         <Text style ={{margin: 20 , color: '#f5f6fa', fontSize: 20, alignSelf: 'center'}}>Logout</Text> 
-        </View> 
+        </View>
+        </TouchableOpacity>
+
+        
         </SafeAreaView>
         )}
+
+
 }
 
 export default ProfileScreen;
