b6109f96fe909a98c55a6dac52aa29050844b536
szojohn <84329469+szojohn@users.noreply.github.com>
2021-05-17 21:54:44 +0800

    Update logout.js

--- src/backend/logout.js
+++ src/backend/logout.js
@@ -1,11 +1,27 @@
-import * as firebase from "firebase";
-import "firebase/firestore";
-import {Alert} from "react-native";
+import React from 'react';
+import { Alert } from "react-native";
+import { clearValues } from './Verification'
 
-export async function loggingOut() {
-  try {
-    await firebase.auth().signOut();
-  } catch (err) {
-    Alert.alert('There is something wrong!', err.message);
+const loggingOut = (navigation) => {
+
+  // alert the user about logging out
+  Alert.alert("Log Out", "Do you want to log out?", [{
+    text: "No",
+    style: "cancel"
+  },
+  {
+    text: "Yes",
+    style: "cancel",
+    onPress: () => {
+
+      // navigate to login screen
+      navigation.navigate('Login');
+
+      // setting user profiles to null
+      clearValues();
+    },
   }
+  ])
 }
+
+export { loggingOut }
